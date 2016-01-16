1aba44a017908213b482659813e7a1541857a45e
Stefano Baghino <stefanobaghino@yahoo.it>
2016-01-16 17:32:03 +0100

    groups.json move here

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var Config = {
   
-  groupsSource: 'https://www.googledrive.com/host/0B5ENAUFZNl-YLXpKTHJ3cmY4ZGc',
+  groupsSource: '/data/groups.json',
   eventsSource: "https://www.googleapis.com/calendar/v3/calendars/3nctrbqaukrc5i8g6j7d54b9q4@group.calendar.google.com/events?key=AIzaSyBP-eVlQhmfD0Ml630CHwWxsv27k14zjfc&maxResults=5&singleEvents=true&orderBy=startTime&timeMin=" + moment().format('YYYY-MM-DD') + "T00%3A00%3A00%2B00%3A00"
 
 };
